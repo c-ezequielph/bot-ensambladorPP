3c66e96bb0842dc27457f28f3273c70919913480
c-ezequielph <55009047+c-ezequielph@users.noreply.github.com>
2020-08-06 15:58:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const client = new Discord.Client();
 require('events').EventEmitter.defaultMaxListeners = 4000;
 /////////////////////////CHANGENAME/////////////////////////////
 
-if (message.content.includes('changeNick')) {
-    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-    message.member.setNickname(message.content.replace('changeNick ', 'Coronao'));
-}
 
 /////////////////////////ENSAMBLADOR////////////////////////////
 
