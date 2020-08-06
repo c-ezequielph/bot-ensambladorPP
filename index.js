c26716a8518fb5ddcb37228ab77e333e8ab86000
c-ezequielph <55009047+c-ezequielph@users.noreply.github.com>
2020-08-06 14:19:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require("dotenv").config()
-const Discord = require('discord.js');
-const client = new Discord.Client();
-require('events').EventEmitter.defaultMaxListeners = 4000;
-
-/////////////////////////ENSAMBLADOR////////////////////////////
-
-
-
-client.on('ready', () => {
-    console.log('Estoy listo!');
-});
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
 
 client.login(process.env.BOT_TOKEN)
