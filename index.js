a6ce8423a60b88ddcaeed30f20ddf602445ad2aa
c-ezequielph <55009047+c-ezequielph@users.noreply.github.com>
2020-08-14 08:47:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
     }
 
     let nombrerol = msg.member.roles.cache.size <= 0 ? "Admin" : msg.member.roles.highest.name;
-    msg.member.setNickname(`${msg.author.username} ${roles[nombrerol]}`
+    msg.member.setNickname(`${msg.author.username} ${roles[nombrerol]}`)
     msg.channel.send("El Moreno te ha dado una mascarilla contra el COVID-19"))
    
     }
