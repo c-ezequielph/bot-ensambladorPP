e324ddcd249441b7f62c4df4d48df6619eb7e539
c-ezequielph <55009047+c-ezequielph@users.noreply.github.com>
2021-02-19 14:46:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,30 @@ client.on('message', msg => {
     }
 })
 
+////////////////////////ANUNCIOS//////////////////////////////
+
+module.exports = async (client, message, args, anuncios) => { //exportamos para discord.js
+  const Discord = require('discord.js')
+  let anuncio = args.slice(0).join(" ") //definimos anuncio
+  var perms = message.member.hasPermission("MANAGE_GUILD"); //definimos los permisos necesarios
+  let autor = `${message.author}` //definimos el autor
+  
+
+  if(!perms) return message.channel.send(`${message.author}`+' **no tienes permisos para hacer esto! Permiso faltante:** ``MANAGE_GUILD``'); //si no tienen permisos
+  if(!anuncio) return message.channel.send(`${message.author}`+' **debes de escribir un anuncio!**') //si no han escrito el anuncio
+  const embed = new Discord.MessageEmbed() //Creamos el embed, customizable a su gusto
+    .setTitle('Anuncios')
+    .setDescription(anuncio)
+    .addField('Anuncio por:', `${message.author}`)
+    .setColor('#00ff11')
+  client.channels.resolve('746951842071576597').send(embed);// colocamos la id del canal del anuncio
+  message.channel.send(`${message.author}`+' **listo, ya di el anuncio!**').then(async(msg) => {
+    setTimeout(() => {
+      msg.delete();
+    }, 5000)
+  })
+}
+
 /////////////////////////ENSAMBLADOR////////////////////////////
 
 //LLANTAS Y PUERTAS
